6fb769301528ff049e4e3266733eaa5385eed23c
lrbridge <lrbridge@users.noreply.github.com>
2015-09-23 00:10:59 -0700

    locking solutions in place with current node expanded/path counts.  they look right so now we'll know if we break something (or fix a bug potentially, in which case we can change the assert values

--- test/com/mp1/Part1ResultsTest.java
+++ test/com/mp1/Part1ResultsTest.java
@@ -1,5 +1,7 @@
 package com.mp1;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.mp1.search.AStarSearch;
@@ -7,41 +9,45 @@ import com.mp1.search.BreadthFirstSearch;
 import com.mp1.search.DepthFirstSearch;
 import com.mp1.search.GreedyBestFirstSearch;
 import com.mp1.search.base.Search;
+import com.mp1.solution.MazeSolution;
 
 /** Easy way to run just this Test class to get all results for part 1 **/
 public class Part1ResultsTest {
 
-	private void runMaze(Search algo, String filename) {
+	private void runMaze(Search algo, String filename, int pathCost, int numExpanded) {
 		System.out.println(algo.getClass().getSimpleName() + " " + filename);
-		System.out.println(algo.solve());
+		MazeSolution solution = algo.solve();
+		System.out.println(solution);
+		assertEquals(solution.pathCost, pathCost);
+		assertEquals(solution.numNodesExpanded, numExpanded);
 	}
 		
 	@Test
 	public void bfs() {
-		this.runMaze(new BreadthFirstSearch("mediumMaze.txt"), "mediumMaze.txt");
-		this.runMaze(new BreadthFirstSearch("bigMaze.txt"), "bigMaze.txt");
-		this.runMaze(new BreadthFirstSearch("openMaze.txt"), "openMaze.txt");
+		this.runMaze(new BreadthFirstSearch("mediumMaze.txt"), "mediumMaze.txt", 42, 224);
+		this.runMaze(new BreadthFirstSearch("bigMaze.txt"), "bigMaze.txt", 62, 720);
+		this.runMaze(new BreadthFirstSearch("openMaze.txt"), "openMaze.txt", 54, 275);
 	}
 	
 	@Test
 	public void dfs() {
-		this.runMaze(new DepthFirstSearch("mediumMaze.txt"), "mediumMaze.txt");
-		this.runMaze(new DepthFirstSearch("bigMaze.txt"), "bigMaze.txt");
-		this.runMaze(new DepthFirstSearch("openMaze.txt"), "openMaze.txt");
+		this.runMaze(new DepthFirstSearch("mediumMaze.txt"), "mediumMaze.txt", 88, 201);
+		this.runMaze(new DepthFirstSearch("bigMaze.txt"), "bigMaze.txt", 232, 603);
+		this.runMaze(new DepthFirstSearch("openMaze.txt"), "openMaze.txt", 162, 274);
 	}
 	
 	@Test
 	public void greedyBestFirstSearch() {
-		this.runMaze(new GreedyBestFirstSearch("mediumMaze.txt"), "mediumMaze.txt");
-		this.runMaze(new GreedyBestFirstSearch("bigMaze.txt"), "bigMaze.txt");
-		this.runMaze(new GreedyBestFirstSearch("openMaze.txt"), "openMaze.txt");
+		this.runMaze(new GreedyBestFirstSearch("mediumMaze.txt"), "mediumMaze.txt", 56, 80);
+		this.runMaze(new GreedyBestFirstSearch("bigMaze.txt"), "bigMaze.txt", 70, 117);
+		this.runMaze(new GreedyBestFirstSearch("openMaze.txt"), "openMaze.txt", 60, 123);
 	}
 	
 	@Test
 	public void aStar() {
-		this.runMaze(new AStarSearch("mediumMaze.txt"), "mediumMaze.txt");
-		this.runMaze(new AStarSearch("bigMaze.txt"), "bigMaze.txt");
-		this.runMaze(new AStarSearch("openMaze.txt"), "openMaze.txt");
+		this.runMaze(new AStarSearch("mediumMaze.txt"), "mediumMaze.txt", 42, 88);
+		this.runMaze(new AStarSearch("bigMaze.txt"), "bigMaze.txt", 62, 273);
+		this.runMaze(new AStarSearch("openMaze.txt"), "openMaze.txt", 54, 123);
 	}
 	
 }
--- test/com/mp1/Part3ResultsTest.java
+++ test/com/mp1/Part3ResultsTest.java
@@ -1,34 +1,40 @@
 package com.mp1;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.mp1.search.AStarSearch;
 import com.mp1.search.AStarWithGhostSearch;
 import com.mp1.search.base.Search;
+import com.mp1.solution.MazeSolution;
 
 public class Part3ResultsTest {
 
-	private void runMaze(Search algo, String filename) {
+	private void runMaze(Search algo, String filename, int pathCost, int numExpanded) {
 		System.out.println(algo.getClass().getSimpleName() + " " + filename);
-		System.out.println(algo.solve());
+		MazeSolution solution = algo.solve();
+		System.out.println(solution);
+		assertEquals(solution.pathCost, pathCost);
+		assertEquals(solution.numNodesExpanded, numExpanded);
 	}
 		
 	@Test
 	public void small() {
-		this.runMaze(new AStarSearch("smallGhost.txt"), "smallGhost.txt");
-		this.runMaze(new AStarWithGhostSearch("smallGhost.txt"), "smallGhost.txt");
+		this.runMaze(new AStarSearch("smallGhost.txt"), "smallGhost.txt", 19, 54);
+		this.runMaze(new AStarWithGhostSearch("smallGhost.txt"), "smallGhost.txt", 21, 122);
 	}
 	
 	@Test
 	public void medium() {
-		this.runMaze(new AStarSearch("mediumGhost.txt"), "mediumGhost.txt");
-		this.runMaze(new AStarWithGhostSearch("mediumGhost.txt"), "mediumGhost.txt");
+		this.runMaze(new AStarSearch("mediumGhost.txt"), "mediumGhost.txt", 24, 27);
+		this.runMaze(new AStarWithGhostSearch("mediumGhost.txt"), "mediumGhost.txt", 26, 30);
 	}
 	
 	@Test
 	public void large() {
-		this.runMaze(new AStarSearch("bigGhost.txt"), "bigGhost.txt");
-		this.runMaze(new AStarWithGhostSearch("bigGhost.txt"), "bigGhost.txt");
+		this.runMaze(new AStarSearch("bigGhost.txt"), "bigGhost.txt", 68, 110);
+		this.runMaze(new AStarWithGhostSearch("bigGhost.txt"), "bigGhost.txt", 70, 131);
 	}
 	
 }
